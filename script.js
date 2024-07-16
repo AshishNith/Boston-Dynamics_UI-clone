b166910b1c0a88786b1d486541b95479e720b915
AshishNith <ranjan.ashish9992@gmail.com>
2024-07-16 09:23:31 +0530

    animation added

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.from(".navbar h1,i,img,.nav-button",{
+    y:50,
+    opacity:0,
+    scale:0,
+    duration:0.2,
+    delay:0.5,
+    stagger:0.05
+})
+
+gsap.from(".Home h1,h2,h3,button",{
+    y:50,
+    opacity:0,
+    duration:1,
+    delay:1,
+    stagger:0.5
+})
